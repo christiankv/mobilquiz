d1d974d9219c154878eb5bbe32a2082eb090f3b2
Simon Heskja <simon.kodehode@gmail.com>
2022-06-14 10:59:37 +0200

    Update App.js

--- mobilquiz/src/App.js
+++ mobilquiz/src/App.js
@@ -1,3 +1,61 @@
+// App and Page functionality 
+
+//choose username then begin.
+    //text field for username, is presistent, stays on each page.
+      //saves to local storage
+        //check local storage for username, if detected skip create username.
+
+
+
+
+//choose theme/quiz/topic or choose tasks which is more of a learning experience.
+  //after username show two options, tasks or quiz. also display a symbol for opening overlay menu with the cheatsheet/s
+    //
+
+
+
+// do the quiz/ three options, one correct, continue to next question, get points at the end but not answers
+    //selecting your answer takes you straight to the next question. quiz should be quick and simple
+        //quiz is a more flowing experience with a faster pace
+
+
+
+//choose tasks, here we want to see what the correct answers was and a more detailed summary at the end.
+    //choose a topic and get a more detailed task oriented display?, more in depth questions and answers
+      //choosing the correct answer gives checkmark, wrong answer shows x and or red background. click next to continue to next question.
+          //task segment will be more static and detailed.
+
+
+
+
+
+
+//cheatsheets just with some generel info like hotkeys, tips and tricks.
+    //cheatsheet will have a button to display an overlay, possibly a topic selection as well with tips and tricks for specific topics.
+      //transparent background? how many tips/topics?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function App() {
