c8dc7c3477b5c9d7b59a4f7a2f8a9f173c49ac5f
christiankv <christian.kodehode@gmail.com>
2022-06-15 13:00:19 +0200

    s

--- mobilquiz/src/moduls/main.jsx
+++ mobilquiz/src/moduls/main.jsx
@@ -1,7 +1,15 @@
 export default function Main(){
+    function Login(){
+        return( <div className="login">
+            <div className="username">
+                <label htmlFor="username">username:<input type="text" name="username" id="username" /></label> 
+            </div>
+            <div className="createuser-button">======></div>
+          </div>)
+    }
 
     return(
-        <div className="main"><p>laget av random folk</p></div>
+        <div className="main"><Login/></div>
 
     )
 }
